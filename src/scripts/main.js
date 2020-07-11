2878f5bc0cc0741a395b573c2b01ecde29809688
DTR <t.terry.rice@gmail.com>
2020-07-11 17:03:55 +0300

    final 1.2.7 (add sctipts)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const contactForm = document.querySelector('.message__form');
+
+function clean(evnt) {
+  evnt.preventDefault();
+  document.location.href = '/';
+}
+
+contactForm.addEventListener('submit', clean);
